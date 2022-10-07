8437240705d302f1c4bca5c2852854d234d26889
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2022-10-07 09:41:46 +0100

    upto joke

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ console.log ("working")
 
 //once that's done, we decide
 
-async function getQuote(){
+async function getPokemon(){
     let response = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
     let data = await response.json();
      console.log(data);
@@ -19,4 +19,28 @@ async function getQuote(){
     
 }
 
-getQuote()
+getPokemon()
+
+async function getQuestions(){
+  let response = await fetch("https://opentdb.com/api.php?amount=10");
+  let data = await response.json();
+   console.log(data);
+  
+}
+
+getQuestions()
+
+async function getJoke(){
+  let response = await fetch("https://icanhazdadjoke.com/", {
+    headers: { accept: "application/json" },
+  });
+  let data = await response.json();
+   console.log(data);
+
+ // let h1 = document.querySelector("#kanye-quote");
+  //h1.textContent = data.quote;
+
+  
+}
+
+getJoke()

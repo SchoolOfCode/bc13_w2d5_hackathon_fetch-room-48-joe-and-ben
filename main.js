109d6f6dbe42ca30a7d2843ce39bff61eea522a3
BenPuusta <benpuusta@gmail.com>
2022-10-07 10:09:51 +0100

    added random selector to pokemon fetch

--- main.js
+++ main.js
@@ -8,39 +8,65 @@ console.log ("working")
 
 //once that's done, we decide
 
+randNumber = Math.floor(Math.random() * 150) + 1;
+
+//^^ choose a random number for the API fetch
+
+fetchRandom = "https://pokeapi.co/api/v2/pokemon/" + (randNumber + "/")
+
+//puts the number into a string and adds the closing string
+
+
+
 async function getPokemon(){
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
+    let response = await fetch(fetchRandom);
     let data = await response.json();
      console.log(data);
 
-   // let h1 = document.querySelector("#kanye-quote");
-    //h1.textContent = data.quote;
+
 
     
 }
 
 getPokemon()
 
-async function getQuestions(){
-  let response = await fetch("https://opentdb.com/api.php?amount=10");
-  let data = await response.json();
-   console.log(data);
+//async function getQuestions(){
+//  let response = await fetch("https://opentdb.com/api.php?amount=10");
+//  let data = await response.json();
+//   console.log(data);
   
-}
+//}
 
-getQuestions()
+//getQuestions()
 
-async function getJoke(){
-  let response = await fetch("https://icanhazdadjoke.com/", {
-    headers: { accept: "application/json" },
-  });
-  let data = await response.json();
-   console.log(data);
+//async function getJoke(){
+//  let response = await fetch("https://icanhazdadjoke.com/", {
+//    headers: { accept: "application/json" },
+//  });
+//  let data = await response.json();
+//   console.log(data);
 
- // let h1 = document.querySelector("#kanye-quote");
-  //h1.textContent = data.quote;
 
   
-}
+//}
 
-getJoke()
+//getJoke()
+
+
+//async function getHello(){
+//    let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/hello`);
+//    let data = await response.json();
+//     console.log(data);
+
+
+    
+//}
+
+//getHello()
+
+
+// next step - get fetch code to randomly change, then test 
+// after this, getting the DOM to work
+// pick out the data from the API to put into the DOM
+// test again
+//
